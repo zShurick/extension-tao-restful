95981a06d100e04e83cc9f72792b386742f9010a
Alexander Zagovorichev <zagovorichev@1pt.com>
2016-02-22 11:41:16 +0300

    turn on Basic Auth

--- proxy/BasicAuthentication.php
+++ proxy/BasicAuthentication.php
@@ -15,7 +15,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * Copyright (c) 2016  (original work) Open Assessment Technologies SA;
- * 
+ *
  * @author Alexander Zagovorichev <zagovorichev@1pt.com>
  */
 
@@ -25,19 +25,18 @@ namespace oat\taoRestAPI\proxy;
 use common_http_Request;
 use common_session_RestSession;
 use oat\oatbox\user\LoginFailedException;
-use oat\taoRestAPI\exception\RestApiException;
 use oat\taoRestAPI\model\AuthenticationInterface;
 use tao_models_classes_HttpBasicAuthAdapter;
 
 class BasicAuthentication implements AuthenticationInterface
 {
     private $adapter;
-    
+
     public function __construct()
     {
         $this->adapter = new tao_models_classes_HttpBasicAuthAdapter(common_http_Request::currentRequest());
     }
-    
+
     public function authenticate()
     {
         try {
@@ -45,8 +44,16 @@ class BasicAuthentication implements AuthenticationInterface
             // login
             $session = new common_session_RestSession($user);
             \common_session_SessionManager::startSession($session);
-        } catch(LoginFailedException $e) {
-            throw new RestApiException($e->getUserMessage(), 401);
+        } catch (LoginFailedException $e) {
+            //throw new RestApiException($e->getUserMessage(), 401);
+            $this->requireLogin();
         }
+    }
+
+    private function requireLogin()
+    {
+        header('HTTP/1.0 401 Unauthorized');
+        header('WWW-Authenticate: Basic realm="' . GENERIS_INSTANCE_NAME . '"');
+        exit(0);
     }
 }
